a109150d4bde09cb24a9f522ec758080b52df5e0
shoaib-shk <shaikh786.shoaib92@gmail.com>
2024-07-18 15:49:58 +0530

    Soln

--- Array/RemoveDuplicatesfromSortedArray-26/code.cpp
+++ Array/RemoveDuplicatesfromSortedArray-26/code.cpp
@@ -6,8 +6,7 @@ public:
             if(nums[i] != nums[i-1]){
                 nums[j] = nums[i];
                 j++;
-            }
-            
+            }   
         }
         return j;
     }
