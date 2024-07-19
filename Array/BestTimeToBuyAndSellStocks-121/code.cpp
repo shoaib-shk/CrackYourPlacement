73d518b8591ffd606d9f4d83ec90626033027d6e
shoaib-shk <shaikh786.shoaib92@gmail.com>
2024-07-19 20:45:12 +0530

    Soln

--- Array/BestTimeToBuyAndSellStocks-121/code.cpp
+++ Array/BestTimeToBuyAndSellStocks-121/code.cpp
@@ -15,44 +15,9 @@ public:
     }
 };
 
-/* Best Approach: 0 ms
-
-int init = [] {
-	cin.tie(nullptr)->sync_with_stdio(false);
-    freopen("user.out", "w", stdout);
-
-    for (string s; getline(cin, s);) {
-        int solution = 0, minPrice = INT_MAX;
-        for (int _i = 1, _n = s.length(); _i < _n; ++_i) {
-            int price = s[_i++] & 15;
-            while ((s[_i] & 15) < 10) price = price * 10 + (s[_i++] & 15);
-            minPrice = min(minPrice, price);
-            solution = max(solution, price - minPrice);
-        }
-cout << solution << '\n';    }
-    return 0;
-}();
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int max=0,min=100000;
-        for(int i=0;i<prices.size();i++)
-        {
-           if(prices[i]<min)
-           min=prices[i];
-
-           if(prices[i]-min>max)
-           max=prices[i]-min;
-        }
-        return max;
-    }
-};
-
-
-*/
 
 /*
-Using sliding window approach (JUST FOR UNDERSTANDING - WILL GIVE TLE)
+Using sliding window approach (JUST FOR UNDERSTANDING Sliding Window approach for this problem :- WILL GIVE TLE)
 
 class Solution {
 public:
